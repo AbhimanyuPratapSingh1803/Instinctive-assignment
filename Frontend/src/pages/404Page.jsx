f10df2ef005b4ba99342da335ebedafd85cdd710
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2024-12-24 13:00:55 +0530

    fixed 404 page

--- Frontend/src/pages/404Page.jsx
+++ Frontend/src/pages/404Page.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+    <div className="h-screen flex px-5 flex-col justify-center items-center bg-gray-900 text-white">
       <div className="text-center">
         <h1 className="text-8xl font-extrabold mb-4 text-indigo-500">404</h1>
         <p className="text-2xl mb-6 opacity-75">Sorry, the page you are looking for does not exist.</p>
